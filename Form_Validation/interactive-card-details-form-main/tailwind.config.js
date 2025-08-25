faf69648abacb3de2a1e1635843ad6c7e733a45b
Gabriel <gabrielngoh07@gmail.com>
2025-08-25 18:08:19 +0100

    card Styling | interactive_card_details_form

--- Form_Validation/interactive-card-details-form-main/tailwind.config.js
+++ Form_Validation/interactive-card-details-form-main/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: 
+    {
+       colors:{
+        activeInputBorderColor : 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+        inputErrorsColorRed :'hsl(0, 100%, 66%)',
+        gray200:'hsl(270, 3%, 87%)',
+        gray400:'hsl(212, 12%, 71%)',
+        purple950:'hsl(278, 68%, 11%)',
+       },
+       fontSize:{
+        fontBasic: '18 px'
+       },
+       backgroundImage:{
+        mainMobile:"url(.../src/images/bg-main-mobile.png)",
+        mainDesktop:"url(./src/images/bg-main-desktop.png)",
+       },
+       fontFamily:
+       {
+        SpaceGrotesk:"SpaceGrotesk",
+      }
+    },
+
   },
   plugins: [],
 }
